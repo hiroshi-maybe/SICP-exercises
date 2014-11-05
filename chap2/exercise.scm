16c227a5672583587d9d18825b70b77e30b034c0
Hiroshi Kori <hiroshi.maybe@gmail.com>
2014-11-05 01:21:18 -0800

    solved Ex 2.30

--- chap2/exercise.scm
+++ chap2/exercise.scm
@@ -217,5 +217,25 @@
 (balanced? (make-mobile (make-branch 10 1000)
 			(make-branch 1 level-3-mobile)))
 
+;; Ex 2.30
+
+;(define (square-tree tree)
+;  (cond ((null? tree) '())
+;	((not (pair? tree)) (square tree))
+;	(else (cons (square-tree (car tree))
+;		    (square-tree (cdr tree))))))
+
+(define (square-tree tree)
+  (map (lambda (subtree)
+	 (if (pair? subtree)
+	     (square-tree subtree)
+	     (square subtree))) tree))  
+
+(square-tree2
+ (list 1
+       (list 2 (list 3 4) 5)
+       (list 6 7)))
+
+
 
 
