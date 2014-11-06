28a9caf2a1ec64fa0986ebc6d5b7f0d4db551d99
Hiroshi Kori <hiroshi.maybe@gmail.com>
2014-11-06 00:40:04 -0800

    solved Ex 2.34

--- chap2/exercise.scm
+++ chap2/exercise.scm
@@ -269,6 +269,15 @@
 (define (length sequence)
   (accumulate (lambda (_ len) (+ len 1)) 0 sequence))
 
+;; Ex 2.34
+
+(define (horner-eval x coefficient-sequence)
+  (accumulate (lambda (this-coeff higher-terms) (+ this-coeff (* x higher-terms)))
+	      0
+	      coefficient-sequence))
+
+; 79
+(horner-eval 2 (list 1 3 0 5 0 1))
 
 
 
