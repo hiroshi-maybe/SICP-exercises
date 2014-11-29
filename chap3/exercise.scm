0dfd044f0ac39ea70b16c31c1dbd258010a3d16a
Hiroshi Kori <hiroshi.maybe@gmail.com>
2014-11-29 14:52:48 -0800

    solve Ex 3.2

--- chap3/exercise.scm
+++ chap3/exercise.scm
@@ -1,5 +1,5 @@
 
-;; Ex 3.1
+;;; Ex 3.1
 
 (define (make-accumulator sum)
   (lambda (acc) 
@@ -11,4 +11,28 @@
 ; 15
 (A 10)
 ; 25
+
+;;; Ex 3.2
+
+(define (make-monitored f)
+  (let ((counter 0))
+    (define (how-many-calls?) counter)
+    (define (reset-count) 
+      (set! counter 0))
+    (define (dispatch m)
+      (cond ((eq? m 'how-many-calls?) (how-many-calls?))
+	    ((eq? m 'reset-count) (reset-count))
+	    (else (set! counter (+ counter 1))
+		  (f m))))
+    dispatch))
+
+(define s (make-monitored sqrt))
+(s 100)
+; 10
+(s 'how-many-calls?)
+; 1
+(s 'reset-count)
+(s 'how-many-calls?)
+; 0
+
 
