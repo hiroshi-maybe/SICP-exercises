5d86cb6de1052c2457394daff3c3a53a4cfc7e85
Hiroshi Kori <hiroshi.maybe@gmail.com>
2014-12-07 13:50:52 -0800

    solved Ex.3.28

--- chap3/exercise.scm
+++ chap3/exercise.scm
@@ -319,3 +319,47 @@
 (put (list 'a 'b 'c2) 'val2+)
 (get (list 'a 'b 'c2))
 ; val2+
+
+;;; Ex 2.38
+
+(define (inverter input output)
+  (define (invert-input)
+    (let ((new-value (logical-not (get-signal input))))
+      (after-delay inverter-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! input invert-input)
+  'ok)
+
+(define (logical-not s)
+  (cond ((= s 0) 1)
+        ((= s 1) 0)
+        (else (error "Invalid signal" s))))
+
+(define (and-gate a1 a2 output)
+  (define (and-action-procedure)
+    (let ((new-value
+           (logical-and (get-signal a1) (get-signal a2))))
+      (after-delay and-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 and-action-procedure)
+  (add-action! a2 and-action-procedure)
+  'ok)
+
+(define (or-gate a1 a2 output)
+  (define (or-action-procedure)
+    (let ((new-value
+           (logical-or (get-signal a1) (get-signal a2))))
+      (after-delay or-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 or-action-procedure)
+  (add-action! a2 or-action-procedure)
+  'ok)
+
+(define (logic-or s1 s2)
+  (if (or (= s1 1) (= s2 1)) 1 0))
+
+
+
