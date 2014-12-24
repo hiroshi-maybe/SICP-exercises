d878fadc3a01d3de059fd039b1e3fbcd6badd5a1
Hiroshi Kori <hiroshi.maybe@gmail.com>
2014-12-23 22:58:42 -0800

    solved Ex 3.75

--- chap3/exercise.scm
+++ chap3/exercise.scm
@@ -801,7 +801,14 @@
 ; 325 425 650 725 845
 
 ;;; Ex 3.74
-
 (define zero-crossings (stream-map sign-change-detector
 				   sense-data
 				   (cons-stream 0 sense-data)))
+
+;;; Ex 3.75
+(define (make-zero-crossings input-stream last-value last-avpt)
+  (let ((avpt (/ (+ (stream-car input-stream) last-value) 2)))
+    (cons-stream (sign-change-detector avpt last-avpt)
+		 (make-zero-crossings (stream-cdr input-stream)
+				      (stream-car input-stream)
+                                      avpt))))
