f8f6d0d29e7a67a3765de3a3660596bd6e925c34
Hiroshi Kori <hiroshi.maybe@gmail.com>
2015-01-07 00:11:59 -0800

    solve Ex 4.4

--- chap4/exercise.scm
+++ chap4/exercise.scm
@@ -24,3 +24,63 @@
 (define (operator-call exp) (cadr exp))
 (define (operands-call exp) (cddr exp))
 
+;;; Ex 4.4
+
+(define (and? exp) (tagged-list? exp 'and))
+(define (or?  exp) (tagged-list? exp 'or))
+
+; new special forms
+(define (eval-4.4-1 exp env)
+  (cond ((self-evaluating? exp) exp)
+	((variable? exp) (lookup-variable-value exp env))
+        ((quoted? exp) (text-of-quotation exp))
+	((and? exp) (eval-and exp env))
+	((or?   or) (eval-or  exp env))
+;; other syntax procedures ;;
+	))
+
+(define (eval-and exp env)
+  (define (expand-and operands)
+    (cond ((null? operands) true)
+	  ((true?  (eval (car operands) env)) (expand-and (cdr operands)))
+	  (else false)))
+  (expand-and (cdr exp) env))
+
+(define (eval-or exp env)
+  (define (expand-or operands)
+    (cond ((null? operands) false)
+	  ((true?  (eval (car operands) env)) true)
+	  (else (expand-or (cdr operands)))))
+  (expand-or (cdr exp) env))
+
+; derived expressions
+(define (eval-4.4-2 exp env)
+  (cond ((self-evaluating? exp) exp)
+	((variable? exp) (lookup-variable-value exp env))
+        ((quoted? exp) (text-of-quotation exp))
+	((and? exp) (eval (and->if exp) env))
+	((or?  exp) (eval (or->if  exp) env))
+;; other syntax procedures ;;
+	))
+
+(define (and-operands exp) (cdr exp))
+(define (or-operands  exp) (cdr exp))
+
+(define (and->if exp)
+  (expand-and-operands (and-operands exp)))
+(define (expand-and-operands operands)
+  (if (null? operands)
+      true
+      (make-if (car operands)
+	       (expand-and-operands (cdr operands))
+	       false)))
+
+(define (or->if exp)
+  (expand-or-operands (or-operands exp)))
+(define (expand-or-operands operands)
+  (if (null? operands)
+      false
+      (make-if (car operands)
+	       true
+	       (expand-or-operands (cdr operands)))))
+
