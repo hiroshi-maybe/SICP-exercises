83e92b570a8bb7444f61a884f161e64a074db0f3
Hiroshi Kori <hiroshi.maybe@gmail.com>
2015-01-31 15:51:13 -0800

    solve Ex 4.57

--- chap4/exercise-query.scm
+++ chap4/exercise-query.scm
@@ -25,6 +25,22 @@
 ;     (not (job ?supervisor (computer . ?div)))
 ;     (job ?supervisor ?job))
 
+;;; Ex 4.57
 
+;(rule (replace ?person1 ?person2)
+;      (and (job ?person1 ?job1)
+;	   (job ?person2 ?job2)
+;	   (or (same ?job1 ?job2)
+;	       (can-do-job ?job1 ?job2))
+;	   (not (same ?person1 ?person2))))
+
+; a
+;(replace ?person (Fect Cy D))
+
+; b
+;(and (replace ?person1 ?person2)
+;     (salary ?person1 ?amount1)
+;     (salary ?person2 ?amount2)
+;     (lisp-value > ?amount1 ?amount2)))
 
 ;(query-driver-loop)
