9daf793bfc584fd797b5931933fd9f32891714e8
Hiroshi Kori <hiroshi.maybe@gmail.com>
2015-01-31 14:43:59 -0800

    Solve Ex 4.56

--- chap4/exercise-query.scm
+++ chap4/exercise-query.scm
@@ -7,3 +7,20 @@
 ; (job ?who (accounting . ?work))
 ; c
 ; (address ?who (Slumerville . ?address))
+
+;;; Ex 4.56
+
+; a
+;(and (supervisor ?who (Bitdiddle Ben))
+;     (address ?who ?where))
+
+; b
+;(and (salary (Bitdiddle Ben) ?amount-ben)
+;     (salary ?who ?amount)
+;     (lisp-value < ?amount ?amount-ben))
+
+; c
+;(and (supervisor ?who ?supervisor)
+;     (not (job ?supervisor (computer . ?div)))
+;     (job ?supervisor ?job))
+
