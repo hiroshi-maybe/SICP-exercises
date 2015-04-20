b51dedfe86926b0da5a503fe041360c0fda7821c
Hiroshi Kori <hiroshi.maybe@gmail.com>
2015-04-20 01:09:01 -0700

    solve Ex 5.13

--- chap5/exercise-regsim.scm
+++ chap5/exercise-regsim.scm
@@ -247,5 +247,82 @@
 (get-register-contents over-ten-machine 'a)
 ; 13
 
+;;; Ex 5.13
+
+(define (make-new-machine)
+  (let ((pc (make-register 'pc))
+	(flag (make-register 'flag))
+	(stack (make-stack))
+	(the-instruction-sequence '()))
+    (let ((the-ops
+	   (list (list 'initialize-stack
+		       (lambda () (stack 'initialize)))))
+	  (register-table
+	   (list (list 'pc pc) (list 'flag flag))))
+      (define (allocate-register name)
+	(if (assoc name register-table)
+	    (error "Multiply defined register: " name)
+	    (set! register-table
+		  (cons (list name (make-register name))
+			register-table)))
+	'register-allocated)
+      (define (lookup-register name)
+	(let ((val (assoc name register-table)))
+	  (if val
+	      (cadr val)
+;;; @Change START@ ;;;
+	      (begin (allocate-register name)
+		     (lookup-register name)))))
+;;; @Change END@ ;;;
+      (define (execute)
+	(let ((insts (get-contents pc)))
+	  (if (null? insts)
+	      'done
+	      (begin
+		((instruction-execution-proc (car insts)))
+		(execute)))))
+      (define (dispatch message)
+	(cond ((eq? message 'start)
+	       (set-contents! pc the-instruction-sequence)
+	       (execute))
+	      ((eq? message 'install-instruction-sequence)
+	       (lambda (seq) (set! the-instruction-sequence seq)))
+	      ((eq? message 'allocate-register) allocate-register)
+	      ((eq? message 'get-register) lookup-register)
+	      ((eq? message 'install-operations)
+	       (lambda (ops) (set! the-ops (append the-ops ops))))
+	      ((eq? message 'stack) stack)
+	      ((eq? message 'operations) the-ops)
+	      (else (error "Unknown request -- MACHINE" message))))
+            dispatch)))
+
+; Omit `register-names` and their allocation
+(define (make-machine ops controller-text)
+  (let ((machine (make-new-machine)))
+;    (for-each (lambda (register-name)
+;		((machine 'allocate-register) register-name))
+;	      register-names)
+    ((machine 'install-operations) ops)
+    ((machine 'install-instruction-sequence)
+     (assemble controller-text machine))
+    machine))
+
+; test code
+(define over-ten-machine-ex
+  (make-machine
+   (list (list '> >))
+   '(test
+     (test (op >) (reg a) (const 10))
+     (branch (label over-ten-done))
+     (add (reg a) (reg b))
+     (goto (label test))
+     over-ten-done)))
+
+(set-register-contents! over-ten-machine-ex 'a 1)
+(set-register-contents! over-ten-machine-ex 'b 5)
+(start over-ten-machine-ex)
+(get-register-contents over-ten-machine-ex 'a)
+; 11
+
 
 
