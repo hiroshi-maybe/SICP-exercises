d67779b7db9b4d8333af87b6531b8402203b8faa
Hiroshi Kori <hiroshi.maybe@gmail.com>
2015-04-21 00:45:07 -0700

    solve Ex 5.15

--- chap5/exercise-regsim.scm
+++ chap5/exercise-regsim.scm
@@ -249,6 +249,7 @@
 
 ;;; Ex 5.13
 
+#|
 (define (make-new-machine)
   (let ((pc (make-register 'pc))
 	(flag (make-register 'flag))
@@ -323,6 +324,79 @@
 (start over-ten-machine-ex)
 (get-register-contents over-ten-machine-ex 'a)
 ; 11
+|#
 
+;;; Ex 5.15
 
+(define (make-new-machine)
+  (let ((pc (make-register 'pc))
+	(flag (make-register 'flag))
+	(stack (make-stack))
+	(the-instruction-sequence '())
+	(instruction-counter 0)) ; @CHANGE
+    (let ((the-ops
+	   (list (list 'initialize-stack
+		       (lambda () (stack 'initialize)))))
+	  (register-table
+	   (list (list 'pc pc) (list 'flag flag))))
+      (define (allocate-register name)
+	(if (assoc name register-table)
+	    (error "Multiply defined register: " name)
+	    (set! register-table
+		  (cons (list name (make-register name))
+			register-table)))
+	'register-allocated)
+      (define (lookup-register name)
+	(let ((val (assoc name register-table)))
+	  (if val
+	      (cadr val)
+	      (error "Unknown register:" name))))
+      (define (execute)
+	(let ((insts (get-contents pc)))
+	  (if (null? insts)
+	      'done
+	      (begin
+		((instruction-execution-proc (car insts)))
+		(set! instruction-counter (+ instruction-counter 1)) ; @CHANGE
+		(execute)))))
+      (define (print-instruction-counter)
+	(newline)
+	(display (list 'instruction-counter  '= instruction-counter))
+	'print-done)
+      (define (dispatch message)
+	(cond ((eq? message 'start)
+	       (set-contents! pc the-instruction-sequence)
+	       (execute))
+	      ((eq? message 'install-instruction-sequence)
+	       (lambda (seq) (set! the-instruction-sequence seq)))
+	      ((eq? message 'allocate-register) allocate-register)
+	      ((eq? message 'get-register) lookup-register)
+	      ((eq? message 'install-operations)
+	       (lambda (ops) (set! the-ops (append the-ops ops))))
+	      ((eq? message 'stack) stack)
+	      ((eq? message 'operations) the-ops)
+	      ((eq? message 'instruction-counter) (print-instruction-counter)) ; @CHANGE
+	      (else (error "Unknown request -- MACHINE" message))))
+      dispatch)))
+
+; test code
+(define gcd-machine
+  (make-machine
+   '(a b t)
+   (list (list 'rem remainder) (list '= =))
+   '(test-b
+     (test (op =) (reg b) (const 0))
+     (branch (label gcd-done))
+     (assign t (op rem) (reg a) (reg b))
+     (assign a (reg b))
+     (assign b (reg t))
+     (goto (label test-b))
+     gcd-done)))
+
+(set-register-contents! gcd-machine 'a 206)
+(set-register-contents! gcd-machine 'b 40)
+(start gcd-machine)
+(get-register-contents gcd-machine 'a)
+(gcd-machine 'instruction-counter)
+; (instruction-counter = 26)
 
