e34808a0a6901d8a8a4f96a35c579d18716ac5b4
Hiroshi Kori <hiroshi.maybe@gmail.com>
2015-05-09 15:16:38 -0700

    solve Ex 5.26

--- chap5/exercise-eceval.scm
+++ chap5/exercise-eceval.scm
@@ -34,6 +34,53 @@ ok
 3
 |#
 
+;;; Ex 5.26
+
+;;;;;;;; naiive recursive call
+#|
+(define (factorial n)
+  (if (= n 1)
+      1
+      (* (factorial (- n 1)) n)))
+
+;;; EC-Eval input:
+(factorial 5)
+(total-pushes = 144 maximum-depth = 28)
+1: 16
+2: 48
+3: 80
+4: 112
+5: 144
+
+; b
+<total-pushes> = 32 * <n> - 16
+
+|#
+
+;;;;;;;; tail recursive
+#|
+(define (factorial-t n)
+  (define (iter product counter)
+    (if (> counter n)
+	product
+	(iter (* counter product)
+	      (+ counter 1))))
+  (iter 1 1))
+
+;;; EC-Eval input:
+(factorial-t 5)
+(total-pushes = 204 maximum-depth = 10)
+1: 64
+2: 99
+3: 134
+4: 169
+5: 204
+
+; b
+<total-pushes> = 35 * <n> + 29
+
+|#
+
 ; start repl
 (define the-global-environment (setup-environment))
 (start eceval)
