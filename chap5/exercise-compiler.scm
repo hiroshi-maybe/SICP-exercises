bc1c9c7bdf0e4fd3d46c1923197e9e5c786e615b
Hiroshi Kori <hiroshi.maybe@gmail.com>
2015-05-30 17:48:55 -0700

    solve Ex 5.40

--- chap5/exercise-compiler.scm
+++ chap5/exercise-compiler.scm
@@ -656,3 +656,63 @@
     (set-val! (addr-offset lexical-addr)
 	      (frame-variables frame)
 	      (frame-values frame))))
+
+;;; Ex 5.40
+
+(define (compile exp target linkage compile-time-env)                        ; compile-time-env
+  (cond ((self-evaluating? exp)
+         (compile-self-evaluating exp target linkage compile-time-env))      ; compile-time-env
+        ((quoted? exp) (compile-quoted exp target linkage compile-time-env)) ; compile-time-env
+        ((variable? exp)
+         (compile-variable exp target linkage compile-time-env))             ; compile-time-env
+        ((assignment? exp)
+         (compile-assignment exp target linkage compile-time-env))           ; compile-time-env
+        ((definition? exp)
+         (compile-definition exp target linkage compile-time-env))           ; compile-time-env
+        ((if? exp) (compile-if exp target linkage compile-time-env))         ; compile-time-env
+        ((lambda? exp) (compile-lambda exp target linkage compile-time-env)) ; compile-time-env
+        ((begin? exp)
+         (compile-sequence (begin-actions exp)
+                           target
+                           linkage
+			   compile-time-env))                                ; compile-time-env
+        ((cond? exp) (compile (cond->if exp) target linkage compile-time-env)) ; compile-time-env
+        ((application? exp)
+         (compile-application exp target linkage compile-time-env))            ; compile-time-env
+	(else
+	 (error "Unknown expression type -- COMPILE" exp))))
+
+(define (compile-lambda exp target linkage compile-time-env) ; compile-time-env
+  (let ((proc-entry (make-label 'entry))
+	(after-lambda (make-label 'after-lambda)))
+    (let ((lambda-linkage
+	   (if (eq? linkage 'next) after-lambda linkage)))
+      (append-instruction-sequences
+       (tack-on-instruction-sequence
+	(end-with-linkage lambda-linkage
+			  (make-instruction-sequence '(env) (list target)
+						     `((assign ,target
+							       (op make-compiled-procedure)
+							       (label ,proc-entry)
+							       (reg env)))))
+	(compile-lambda-body exp proc-entry compile-time-env)) ; compile-time-env
+       after-lambda))))
+
+(define (compile-lambda-body exp proc-entry compile-time-env)
+  (let ((formals (lambda-parameters exp)))
+    (append-instruction-sequences
+     (make-instruction-sequence '(env proc argl) '(env)
+				`(,proc-entry
+				  (assign env (op compiled-procedure-env) (reg proc))
+				  (assign env
+					  (op extend-environment)
+					  (const ,formals)
+					  (reg argl)
+					  (reg env))))
+     (compile-sequence (lambda-body exp) 'val 'return
+		       (extend-compile-time compile-time-env formals)))) ; extend env
+
+; helper
+(define (extend-compile-time-env env frame)
+  (cons frame env))
+
